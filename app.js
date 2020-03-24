6833793a9f5f49c5cbe2a35a129b9ae6f317a2ad
AnmolKumar Shah <anmolshah.20k@gmail.com>
2020-03-24 23:20:53 +0530

    Setting up first event listner i.e click and keypress

--- app.js
+++ app.js
@@ -8,6 +8,25 @@ var budgetController = (function(){
 /* UI controller */
 var UIController = (function(){
     
+    var ctrlAddItem = function(){
+        /* Get field input data */
+        
+        /* Add the item to the budget controller */
+        
+        /* Add the item to the UI */
+        
+        /* Calculate the budget */
+        
+        /* Display the budget to the UI */        
+    }
+    document.querySelector(".add__btn").addEventListener('click', ctrlAddItem)
+    
+    document.addEventListener('keypress', function(event){
+            
+        if(event.keyCode === 13 || event.which === 13)
+            ctrlAddItem();
+    });
+    
 })();
 
 /* Controller */
