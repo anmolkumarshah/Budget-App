2058365a33ec6bd36f5302c7260b3073459682ce
AnmolKumar Shah <anmolshah.20k@gmail.com>
2020-03-24 22:59:52 +0530

    Modules structure added

---
+++ app.js
@@ -0,0 +1,18 @@
+/* Budget controller */
+var budgetController = (function(){
+   
+    
+    
+})();
+
+/* UI controller */
+var UIController = (function(){
+    
+})();
+
+/* Controller */
+var controller = (function(budgetCtrl, UICtrl){
+    
+    
+})(budgetController, UIController);
+
